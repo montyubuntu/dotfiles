40e42c67d841372ae80342101e1290f67cf27116
Jannick Sikkema <jannick.sikkema@topicus.nl>
2017-04-20 16:16:01 +0200

    Added check for pip package

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -3,6 +3,10 @@ mkdir -p $DOTFILES/caches/vim
 
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
-  pip install flake8
+  if [ -x /usr/bin/pip ]; then
+    pip install flake8
+  else
+    echo "Unable to find pip, the flake8 vim plugin might not work, please install pip on your system and run: 'pip install flake8'."
+  fi
   vim +PlugUpgrade +PlugUpdate +qall
 fi
